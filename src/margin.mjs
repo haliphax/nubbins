37fef3ef532ad5a32a3c3e36eb00241d258fb8c0
haliphax <haliphax@gmail.com>
2024-11-04 16:27:26 -0600

    ✨ x/y margin/padding

--- src/margin.mjs
+++ src/margin.mjs
@@ -3,12 +3,15 @@ import { writeFileSync } from "fs";
 export const marginGenerator = () => {
 	const output = [];
 
-	for (let side of ["top", "bottom", "right", "left"]) {
-		for (let size of ["xxs", "xs", "s", "m", "l", "xl", "xxl"]) {
-			output.push(`
-			.m${side[0]}-${size} { margin-${side}: var(--space-${size}); }
-		`);
+	for (let size of ["xxs", "xs", "s", "m", "l", "xl", "xxl"]) {
+		for (let side of ["top", "bottom", "right", "left"]) {
+			output.push(
+				`.m${side[0]}-${size} { margin-${side}: var(--space-${size}); }\n`,
+			);
 		}
+
+		output.push(`.mx-${size} { margin-inline: var(--space-${size}); }\n`);
+		output.push(`.my-${size} { margin-block: var(--space-${size}); }\n`);
 	}
 
 	writeFileSync("src/margin.css", output.join(""), { encoding: "utf-8" });
--- src/padding.mjs
+++ src/padding.mjs
@@ -3,12 +3,15 @@ import { writeFileSync } from "fs";
 export const paddingGenerator = () => {
 	const output = [];
 
-	for (let side of ["top", "bottom", "right", "left"]) {
-		for (let size of ["xxs", "xs", "s", "m", "l", "xl", "xxl"]) {
-			output.push(`
-			.p${side[0]}-${size} { padding-${side}: var(--space-${size}); }
-		`);
+	for (let size of ["xxs", "xs", "s", "m", "l", "xl", "xxl"]) {
+		for (let side of ["top", "bottom", "right", "left"]) {
+			output.push(
+				`.p${side[0]}-${size} { padding-${side}: var(--space-${size}); }\n`,
+			);
 		}
+
+		output.push(`.px-${size} { padding-inline: var(--space-${size}); }\n`);
+		output.push(`.py-${size} { padding-block: var(--space-${size}); }\n`);
 	}
 
 	writeFileSync("src/padding.css", output.join(""), { encoding: "utf-8" });
